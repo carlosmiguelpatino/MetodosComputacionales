4ac961305e2489b947954152510002baf410e303
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2015-02-27 22:59:30 -0500

    punto a

--- Tarea3/subiendo_escaleras.py
+++ Tarea3/subiendo_escaleras.py
@@ -1,24 +1,48 @@
-import sys
-
-n = sys.argv[1]
-posibilidades = 0
-
-
 """La solucion al problema es crear una serie de fibonacci porque
-para saber las posibilidades hasta un escalón n se deben tener en cuenta
-las posibilidades de llegar al anterior escalón y sumarlas. Tomando
-este principio desde el escalón uno se obtiene una serie de fibonacci
-ya que las posibilidades que hay para llegar un escalón n depende de
+para saber las posibilidades hasta un escalon n se deben tener en cuenta
+las posibilidades de llegar al anterior escalon y sumarlas. Tomando
+este principio desde el escalon uno se obtiene una serie de fibonacci
+ya que las posibilidades que hay para llegar un escalon n depende de
 las posibilidades de llegar a los dos escalones anteriores. Se toman los
 dos escalones anteriores ya que solo se pueden subir uno o dos
 escalones a la vez."""
 
-if n == 0:
-	print "No hay escalones para subir"
+def posibilidades_escalera(n):
 
-if n == 1:
-	print "Hay una "
+	if n == 0:
+		print "No hay escalones para subir"
+	elif n == 1:
+		posibilidades = 1
+	elif n == 2:
+		posibilidades = 2
+	else:
+		posibilidades = posibilidades_escalera(n-1) + posibilidades_escalera(n-2)
 
+	return posibilidades
 
+#Para 13 escalones
 
-print "Hay ",posibilidades, "de subir una escalera de ", n, "escalones"
+posibilidades = posibilidades_escalera(13)
+print "Hay",posibilidades, "maneras de subir una escalera de 13 escalones"
+
+#Para 15 escalones
+
+posibilidades = posibilidades_escalera(15)
+print "Hay",posibilidades, "maneras de subir una escalera de 15 escalones"
+
+def escaleras(A, B):
+	resultados = []
+	for i in range(len(A)):
+		a = A[i]
+		b = B[i]
+		resultado = posibilidades_escalera(a)
+		resultado = resultado % (2*b)
+		resultados.append(resultado)
+		
+
+	return resultados
+
+A = [4, 4, 5, 5, 1]
+B = [3, 2, 4, 3, 1]
+
+print escaleras(A, B)
